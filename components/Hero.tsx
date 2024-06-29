b30e91706c282e6710c165dfeb0bd1a296afbb41
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-06-29 16:33:09 +0700

    update portfolio

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Let's build something awesome together.
+            Let&apos;s build something awesome together.
           </p>
 
           <a href="#about">
