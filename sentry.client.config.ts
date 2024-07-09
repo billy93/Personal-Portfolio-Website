ddd00d164e270b83b0f532e239a54fc681b08465
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-07-09 12:51:28 +0700

    update sentry config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://21ae68c8173074dc682b5a5c6d51f7ca@o4507570177638400.ingest.us.sentry.io/4507570185568256",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://21ae68c8173074dc682b5a5c6d51f7ca@o4507570177638400.ingest.us.sentry.io/4507570185568256",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -13,7 +13,7 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
   
 });
